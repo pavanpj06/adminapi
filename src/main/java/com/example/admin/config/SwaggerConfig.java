2046bb0ec3ec488e472192ba62f4dae37c96754b
pavanpj06 <pavankumarreddy9222@gmail.com>
2025-05-13 15:56:10 +0530

    Delete src directory

--- src/main/java/com/example/admin/config/SwaggerConfig.java
+++
@@ -1,5 +0,0 @@
-package com.example.admin.config;
-
-public class SwaggerConfig {
-
-}
--- src/main/java/com/example/admin/exceptions/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.admin.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    
-
-	public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
